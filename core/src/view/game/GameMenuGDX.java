6653d2e9b5b69191e460bb4402d05daa0390c5ef
mohaliza138 <mohaliza138@gmail.com>
2023-06-23 13:54:47 +0330

    select debugged

--- core/src/view/game/GameMenuGDX.java
+++ core/src/view/game/GameMenuGDX.java
@@ -46,9 +46,11 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
     private Skin skin;
     private Dialog dialog;
     private WindowWithTopRightCornerCloseButton windowWithTopRightCornerCloseButton;
-
-
-    public GameMenuGDX(int mapSize) {
+    private int draggingTileI;
+    private int draggingTileJ;
+    
+    
+    public GameMenuGDX (int mapSize) {
         this.mapSize = mapSize;
         MAP_HEIGHT = 16 * mapSize;
         MAP_WIDTH = 30 * mapSize;
@@ -59,13 +61,13 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         screenWidth = gd.getDisplayMode().getWidth();
         screenHeight = gd.getDisplayMode().getHeight() - 150;
-        currentTileJ = currentTileI = -1;
+        draggingTileI = draggingTileJ = currentTileJ = currentTileI = -1;
         hoverTime = 0;
     }
-
-
+    
+    
     @Override
-    public void create() {
+    public void create () {
         stage = new Stage();
         stage1 = new Stage();
         windowWithTopRightCornerCloseButton = new WindowWithTopRightCornerCloseButton();
@@ -75,19 +77,18 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         windowWithTopRightCornerCloseButton.setMovable(false);
         windowWithTopRightCornerCloseButton.setPosition(0, 0);
         stage.addActor(windowWithTopRightCornerCloseButton);
-
+        
         InputMultiplexer multiplexer = new InputMultiplexer();
         multiplexer.addProcessor(this);
         multiplexer.addProcessor(stage);
         Gdx.input.setInputProcessor(multiplexer);
-
+        
         spriteBatch = new SpriteBatch();
-
-
-
+        
+        
         skin = new Skin(Gdx.files.internal("assets/default.json"));
         dialog = new Dialog("Warning", skin);
-
+        
         textureAssets = new AssetManager();
         textureAssets.load("Tiles.atlas", TextureAtlas.class);
         textureAssets.finishLoading();
@@ -99,17 +100,17 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         towerAssets.finishLoading();
         
         towerAtlas = towerAssets.get("Buildings.atlas");
-
+        
         map2dAssets = new AssetManager();
         map2dAssets.load("miniMap.atlas", TextureAtlas.class);
         map2dAssets.finishLoading();
-
+        
         map2dAtlas = map2dAssets.get("miniMap.atlas");
-
-
+        
+        
         sprites = new Sprite[mapSize][mapSize];
         sprites2d = new Sprite[mapSize][mapSize];
-
+        
         camera = new OrthographicCamera();
         camera.zoom = 1.0f;
         camera.setToOrtho(false, screenWidth, screenHeight);
@@ -123,12 +124,12 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
                 sprites2d[i][j] = new Sprite(map2dAtlas.findRegion((i > 20 && i < 26) ? "river" : (j > 81 && j < 89)
                         ? "burnt" : "wheat"));
             }
-
+        
         sprites[10][15] = new Sprite(towerAtlas.findRegion("tower"));
         sprites[10][15].setPosition(getXFromIAndJ(10, 15), getYFromIAndJ(10, 15));
-
+        
         towers = new ArrayList<>();
-
+        
         for (int i = 0; i < mapSize; i++)
             for (int j = 0; j < mapSize; j++) {
                 if (i % 10 == 8 && (i + j) % 99 == 4) {
@@ -138,9 +139,9 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
                 }
             }
     }
-
+    
     @Override
-    public void render() {
+    public void render () {
         if (currentTileJ == getPositionJ(getCursorX(), getCursorY()) && currentTileI == getPositionI(getCursorX(),
                 getCursorY())) {
             if (hoverTime <= 1f && hoverTime >= 0f) {
@@ -149,7 +150,7 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
                 //TODO: implement hover function
                 hoverTime = -1f;
                 displayDialog();
-
+                
             }
         } else {
             hoverTime = 0f;
@@ -157,8 +158,7 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
             currentTileI = getPositionI(getCursorX(), getCursorY());
             if (currentTileJ > 99 || currentTileJ < 0 || currentTileI > 99 || currentTileI < 0)
                 currentTileJ = currentTileI = -1;
-            if (!stage1.getActors().isEmpty())
-                stage1.getActors().get(0).remove();
+            if (!stage1.getActors().isEmpty()) stage1.getActors().get(0).remove();
         }
         handleInput();
         Gdx.gl.glClearColor(0.5f, 0.8f, 0.5f, 1);
@@ -175,13 +175,13 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
             tower.draw(spriteBatch);
         }
         spriteBatch.end();
-
-
+        
+        
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
         stage1.act(Gdx.graphics.getDeltaTime());
         stage1.draw();
-
+        
         spriteBatch.begin();
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
@@ -193,22 +193,22 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         }
         spriteBatch.end();
     }
-
+    
     @Override
-    public void dispose() {
+    public void dispose () {
         spriteBatch.dispose();
     }
-
-    public int getPositionJ(float x, float y) {
+    
+    public int getPositionJ (float x, float y) {
         double atan = Math.atan(y / (x + MAP_WIDTH / 2));
         return (int) (((Math.cos(Math.atan((float) 8 / 15) - atan) - Math.sin(Math.atan((float) 8 / 15) - atan) / Math.tan(2 * Math.atan((float) 8 / 15))) * Math.sqrt((x + MAP_WIDTH / 2) * (x + MAP_WIDTH / 2) + y * y)) / Math.sqrt(289));
     }
-
-    public int getPositionI(float x, float y) {
+    
+    public int getPositionI (float x, float y) {
         return (int) ((Math.sin(Math.atan((float) 8 / 15) - Math.atan(y / (x + MAP_WIDTH / 2))) * Math.sqrt((x + MAP_WIDTH / 2) * (x + MAP_WIDTH / 2) + y * y)) / (Math.sqrt(289) * Math.sin(2 * Math.atan((float) 8 / 15))));
     }
-
-    public void fitMap() {
+    
+    public void fitMap () {
         if (camera.position.x > (MAP_WIDTH / 2 - camera.viewportWidth * camera.zoom / 2))
             camera.position.x = MAP_WIDTH / 2 - camera.viewportWidth * camera.zoom / 2;
         else if (camera.position.x < -(MAP_WIDTH / 2 - camera.viewportWidth * camera.zoom / 2))
@@ -216,10 +216,11 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         if (camera.position.y > MAP_HEIGHT / 2 - camera.viewportHeight * camera.zoom / 2)
             camera.position.y = MAP_HEIGHT / 2 - camera.viewportHeight * camera.zoom / 2;
         else if (camera.position.y < -windowWithTopRightCornerCloseButton.getHeight() - (MAP_HEIGHT / 2 - camera.viewportHeight * camera.zoom / 2))
-            camera.position.y = -windowWithTopRightCornerCloseButton.getHeight() - (MAP_HEIGHT / 2 - camera.viewportHeight * camera.zoom / 2);
+            camera.position.y =
+                    -windowWithTopRightCornerCloseButton.getHeight() - (MAP_HEIGHT / 2 - camera.viewportHeight * camera.zoom / 2);
     }
-
-    private void handleInput() {
+    
+    private void handleInput () {
         float deltaTime = Gdx.graphics.getDeltaTime();
         float cameraSpeed = CAMERA_SPEED * deltaTime;
         if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT) && camera.position.y - getCursorY() < MAP_HEIGHT / 4 - windowWithTopRightCornerCloseButton.getHeight()) {
@@ -229,17 +230,17 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
             fitMap();
         }
     }
-
-    public float getCursorX() {
+    
+    public float getCursorX () {
         return camera.zoom * (Gdx.input.getX() - screenWidth / 2) + camera.position.x;
     }
-
-    public float getCursorY() {
+    
+    public float getCursorY () {
         return camera.zoom * (screenHeight / 2 - Gdx.input.getY()) + camera.position.y;
     }
-
+    
     @Override
-    public boolean keyDown(int keycode) {
+    public boolean keyDown (int keycode) {
         if (keycode == Input.Keys.NUM_2 && camera.zoom < 5) {
             camera.zoom += 0.1;
         }
@@ -248,16 +249,16 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         }
         return false;
     }
-
-    public float getXFromIAndJ(int i, int j) {
+    
+    public float getXFromIAndJ (int i, int j) {
         return ((j + i) * 15) - (MAP_WIDTH / 2);
     }
-
-    public float getYFromIAndJ(int i, int j) {
+    
+    public float getYFromIAndJ (int i, int j) {
         return (j - i - 1) * 8;
     }
-
-    public void displayDialog() {
+    
+    public void displayDialog () {
         dialog = new Dialog("", skin);
         dialog.text("Are you sure you want to quit?");
         stage1.addActor(dialog);
@@ -266,70 +267,64 @@ public class GameMenuGDX extends ApplicationAdapter implements InputProcessor {
         dialog.show(stage1);
         dialog.setPosition(Gdx.input.getX(), screenHeight - Gdx.input.getY());
     }
-
+    
     @Override
-    public boolean keyUp(int i) {
+    public boolean keyUp (int i) {
         return false;
     }
-
+    
     @Override
-    public boolean keyTyped(char c) {
+    public boolean keyTyped (char c) {
         return false;
     }
-
+    
     @Override
-    public boolean touchDown(int i, int i1, int i2, int i3) {
+    public boolean touchDown (int i, int i1, int i2, int i3) {
         if (i3 == 0 && Gdx.input.isKeyPressed(Input.Keys.S)) {
             int k = getPositionI(getCursorX(), getCursorY());
             int j = getPositionJ(getCursorX(), getCursorY());
             if (sprites[k][j].getColor().b == 0.5 && sprites[k][j].getColor().g == 0.5 && sprites[k][j].getColor().r == 0.5)
                 sprites[k][j].setColor(1, 1, 1, 1);
-            else
-                sprites[k][j].setColor(0.5f, 0.5f, 0.5f,1);
+            else sprites[k][j].setColor(0.5f, 0.5f, 0.5f, 1);
             return true;
         }
         return false;
     }
-
-
+    
+    
     @Override
-    public boolean touchUp(int i, int i1, int i2, int i3) {
+    public boolean touchUp (int i, int i1, int i2, int i3) {
         return false;
     }
-
+    
     @Override
-    public boolean touchDragged(int i, int i1, int i2) {
+    public boolean touchDragged (int i, int i1, int i2) {
         if (i2 == 0 && Gdx.input.isKeyPressed(Input.Keys.S)) {
             int x1 = Gdx.input.getX();
             int y1 = Gdx.input.getY();
             Vector3 input = new Vector3(x1, y1, 0);
+            int iOfTile = getPositionI(getCursorX(), getCursorY());
+            int jOfTile = getPositionJ(getCursorX(), getCursorY());
             camera.unproject(input);
-            for (int k = 0; k < mapSize; k++) {
-                for (int j = 0; j < mapSize; j++) {
-                    Sprite sprite = sprites[k][j];
-                    if (input.x > sprite.getX() && input.x < sprite.getX() + sprite.getWidth()) {
-                        if (input.y > sprite.getY() && input.y < sprite.getY() + sprite.getHeight()) {
-                            if (sprites[k][j].getColor().b == 0.5 && sprites[k][j].getColor().g == 0.5 && sprites[k][j].getColor().r == 0.5 && currentTileI != getPositionJ(getCursorX(), getCursorY()) && currentTileJ != getPositionI(getCursorX(),
-                                    getCursorY()))
-                                sprites[k][j].setColor(1, 1, 1, 1);
-                            else
-                                sprites[k][j].setColor(0.5f, 0.5f, 0.5f, 1);
-                            //return true;
-                        }
-                    }
-                }
+            Sprite sprite = sprites[iOfTile][jOfTile];
+            if (iOfTile != draggingTileI || jOfTile != draggingTileJ) {
+                if (sprite.getColor().b == 0.5 && sprite.getColor().g == 0.5 && sprite.getColor().r == 0.5)
+                    sprite.setColor(1, 1, 1, 1);
+                else sprite.setColor(0.5f, 0.5f, 0.5f, 1);
             }
+            draggingTileI = iOfTile;
+            draggingTileJ = jOfTile;
         }
         return false;
     }
-
+    
     @Override
-    public boolean mouseMoved(int i, int i1) {
+    public boolean mouseMoved (int i, int i1) {
         return false;
     }
-
+    
     @Override
-    public boolean scrolled(float v, float v1) {
+    public boolean scrolled (float v, float v1) {
         return false;
     }
 }
